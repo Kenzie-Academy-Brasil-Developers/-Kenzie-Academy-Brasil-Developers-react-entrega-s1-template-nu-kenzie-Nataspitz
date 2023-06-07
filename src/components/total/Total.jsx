5f669bbf75c463885d1a096ab2a78507874edb5f
Nata Spitz Alves <nataspitz81@gmail.com>
2023-06-07 08:29:17 -0300

    feat: cria remoção de div do total

--- src/components/total/Total.jsx
+++ src/components/total/Total.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import { StyleTotal } from "./StyleTotal";
 
 export function Total({  transitionList }) {
@@ -12,12 +12,20 @@ export function Total({  transitionList }) {
     },0)
 
     return(
-        <StyleTotal>
-            <div>
-                <h3>Valor Total:</h3>     
-                <span>{`R$ ${totalValue.toFixed(2)}`}</span>
-            </div>
-            <p>O valor referido ao seu saldo</p>
-        </StyleTotal>
+        <>
+            {
+                totalValue != 0 ?(
+                <StyleTotal>
+            
+                    <div>
+                        <h3>Valor Total:</h3>     
+                        <span>{`R$ ${totalValue.toFixed(2)}`}</span>
+                    </div>
+                    <p>O valor referido ao seu saldo</p>
+                </StyleTotal>
+                ) : null
+            }
+        </>
+
     )
 }
